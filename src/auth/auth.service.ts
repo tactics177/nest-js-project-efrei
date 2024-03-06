321b5627fe2ecf132400b51089b14523deb58474
Ali Loudagh <63974953+tactics177@users.noreply.github.com>
2024-03-06 14:21:36 +0000

    update

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,36 +3,55 @@ import { User } from './user.model';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import mongoose from 'mongoose';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
-    constructor(@InjectModel(User.name) private userModel: Model<User>) { }
+  constructor(@InjectModel(User.name) private userModel: Model<User>) {}
 
-    async login(loginDto: { username: string, password: string }): Promise<any> {
-        const { username, password } = loginDto;
+  async login(loginDto: { username: string; password: string }): Promise<any> {
+    const { username, password } = loginDto;
 
-        // Find user by username
-        const user = await this.userModel.findOne({ username }).exec();
+    // Find user by username
+    const user = await this.userModel.findOne({ username }).exec();
 
-        // Check if user exists and password matches
-        if (user && user.password === password) {
-            // Authentication successful
-            return { message: 'Login successful' };
-        } else {
-            // Authentication failed
-            throw new Error('Invalid username or password');
-        }
+    // Check if user exists and verify the password
+    if (user && (await bcrypt.compare(password, user.password))) {
+      // Authentication successful
+      return { message: 'Login successful' };
+    } else {
+      // Authentication failed
+      throw new Error('Invalid username or password');
+    }
+  }
+
+  async register(registerDto: {
+    username: string;
+    password: string;
+  }): Promise<any> {
+    // Check if username already exists
+    const existingUser = await this.userModel
+      .findOne({ username: registerDto.username })
+      .exec();
+    if (existingUser) {
+      throw new Error('Username already exists');
     }
 
-    async register(registerDto: { username: string, password: string }): Promise<any> {
-        // Implement registration logic here
-        const newUser = new this.userModel({ username: registerDto.username, password: registerDto.password, _id: new mongoose.Types.ObjectId() });
-        
-        try {
-            const savedUser = await newUser.save(); // Save the user to the database
-            return { message: 'Registration successful' };
-        } catch (error) {
-            throw new Error('Failed to register user');
-        }
+    // Hash the password
+    const hashedPassword = await bcrypt.hash(registerDto.password, 10); // 10 is the saltRounds
+
+    // Create a new user with the hashed password
+    const newUser = new this.userModel({
+      username: registerDto.username,
+      password: hashedPassword,
+      _id: new mongoose.Types.ObjectId(),
+    });
+
+    try {
+      const savedUser = await newUser.save(); // Save the user to the database
+      return { message: 'Registration successful' };
+    } catch (error) {
+      throw new Error('Failed to register user');
     }
+  }
 }
