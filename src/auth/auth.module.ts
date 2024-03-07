4369610eb9d27da06866d339b38a40e35b7a86b4
Ali Loudagh <63974953+tactics177@users.noreply.github.com>
2024-03-07 14:53:00 +0000

    update

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -12,7 +12,7 @@ import { jwtConstants } from './constants';
     JwtModule.register({
       global: true,
       secret: jwtConstants.secret,
-      signOptions: { expiresIn: '60s' },
+      signOptions: { expiresIn: '60s' }, // If you wait 60 seconds after authenticating before attempting a GET /auth/profile request, you'll receive a 401 Unauthorized response
     }),
   ],
   providers: [AuthService],
